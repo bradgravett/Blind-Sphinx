9c63003ff8f5b333483a8ca0e58202da665362ec
Brad Gravett <bradg@retailsuccess.com>
2026-05-10 22:11:13 -0500

    Initial commit, with forge-game dependency

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.composeHotReload)
+}
+
+kotlin {
+    jvm()
+    
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.compose.runtime)
+            implementation(libs.compose.foundation)
+            implementation(libs.compose.material3)
+            implementation(libs.compose.ui)
+            implementation(libs.compose.components.resources)
+            implementation(libs.compose.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodelCompose)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+        jvmMain.dependencies {
+            implementation(compose.desktop.currentOs)
+            implementation(libs.kotlinx.coroutinesSwing)
+            implementation("forge:forge-game:2.0.13-SNAPSHOT")
+        }
+    }
+}
+
+
+compose.desktop {
+    application {
+        mainClass = "org.bradgravett.blindsphinx.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "org.bradgravett.blindsphinx"
+            packageVersion = "1.0.0"
+        }
+    }
+}
